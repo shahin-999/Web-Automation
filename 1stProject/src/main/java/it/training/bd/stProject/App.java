258658700bc5c59d7d2129a61752dcd83d0fb0d3
shahin-999 <52136857+shahin1998@users.noreply.github.com>
2022-03-01 02:47:15 +0600

    1st commit

---
+++ 1stProject/src/main/java/it/training/bd/stProject/App.java
@@ -0,0 +1,13 @@
+package it.training.bd.stProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
